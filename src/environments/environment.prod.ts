c7216350765f034220f8f0911f8e643f6a9ff593
arsen-movsesyan <arsen.movsesyan@gmail.com>
2023-02-28 14:03:18 -0800

    Fixed API address added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true
 };
 
-export const serverHost = 'https://fraction-calculator-339106.wm.r.appspot.com';
+// export const serverHost = 'https://fraction-calculator-339106.wm.r.appspot.com';
+export const serverHost = 'http://34.122.11.100';
 export const serverPort = "";
